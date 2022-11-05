2af50df71a1abdb29a81c188b0aec32706d5e65a
GroupFour77 <sevenseven403@yahoo.com>
2022-11-05 20:06:53 +0800

    initial set up of App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import * as React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
 function App() {
-  return <div>Binhi</div>;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#ffffff",
+      },
+      secondary: {
+        main: "#000000",
+      },
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>Binhi</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
