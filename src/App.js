2199dec3d012a378de273f386defcac87e9cce40
angge28 <angelicagucela28@gmail.com>
2022-11-07 09:51:23 +0800

    login-layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+//Pages
+import { LoginPage } from "./pages/shared/LoginPage/LoginPage";
+
 function App() {
-  return <div>Binhi</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
