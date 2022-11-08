572a64e1b4d5fc2d09f232d3784e6ec578c60000
angge28 <angelicagucela28@gmail.com>
2022-11-08 11:40:25 +0800

    login-not-yet-functioning

---
+++ src/pages/buyer/BuyerDashboardPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BuyerDashboardPage = () => {
+  return (
+    <div>BuyerDashboardPage</div>
+  )
+}
+
+export default BuyerDashboardPage
---
+++ src/pages/farmer/FarmerDashboardPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function FarmerDashboardPage () {
+  return (
+    <div>FarmerDashboardPage</div>
+  )
+}
+
