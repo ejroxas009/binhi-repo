567a90695ef33d2a0b146f0d7cf461563691a2e7
GroupFour77 <sevenseven403@yahoo.com>
2022-11-15 16:00:08 +0800

    buyer-sidebar partially done

--- src/pages/buyer/MarketPlacePage.jsx
+++ src/pages/buyer/MarketPlacePage.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import * as adsService from "../../service/buyer/AdvertisementService";
 import MarketPlace from "../../component/shared/MarketPlace";
+import BuyerSidebar from "../../component/buyer/BuyerSidebar";
+import Appbar from "../../component/shared/appbar/Appbar";
+import Grid from "@mui/material/Grid";
 
 const MarketPlacePage = () => {
   const [adsList, setAdsList] = useState();
@@ -21,13 +24,23 @@ const MarketPlacePage = () => {
   }, [adsListToggle]);
   return (
     <>
-      {adsList && (
-        <MarketPlace
-          adsList={adsList}
-          onSetAdsListToggle={setAdsListToggle}
-          adsListToggle={adsListToggle}
-        />
-      )}
+      <Grid container>
+        <Grid item xs={12}>
+          <Appbar />
+        </Grid>
+        <Grid item md={3}>
+          <BuyerSidebar />
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12} sx={{ marginTop: 10 }}>
+          {adsList && (
+            <MarketPlace
+              adsList={adsList}
+              onSetAdsListToggle={setAdsListToggle}
+              adsListToggle={adsListToggle}
+            />
+          )}
+        </Grid>
+      </Grid>
     </>
   );
 };
