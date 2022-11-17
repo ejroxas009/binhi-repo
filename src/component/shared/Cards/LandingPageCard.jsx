aa1edc43fe15a7e3a5044871b516f4e00fb69c82
JohnArima <john.salangsang20@gmail.com>
2022-11-18 01:40:40 +0800

    new commit

--- src/component/shared/Cards/LandingPageCard.jsx
+++ src/component/shared/Cards/LandingPageCard.jsx
@@ -6,27 +6,41 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import image from "../../../assets/images/Logo.png";
+import { Link } from 'react-router-dom';
 
 export default function LandingPageCard() {
   return (
-    <Card sx={{ marginLeft:10, height: 500, width: 500, borderRadius: 5 }}>
+    <Card sx={{ marginLeft:20, height: 500, width: 500, borderRadius: 5 }}>
       <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image={image}
-      />
+          component="img"
+          height="294"
+          width="194"
+          image={image}
+          alt="Image"
+        />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          BINHI
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Tagline
-        </Typography>
+
+      <Typography variant = "h4" textAlign="center" sx={{justifyContent:"center"}}>
+        Bridging the Gap between Farm and Market
+      </Typography>
       </CardContent>
-      <CardActions sx={{justifyContent: "center", marginTop:20}}>
-        <Button variant="contained">Login</Button>
-        <Button variant="outlined" >Register</Button>
+      <CardActions sx={{justifyContent: "center",}}>
+        <Button 
+        size="large" 
+        variant="contained" 
+        LinkComponent={Link} 
+        to="/login" 
+        sx={{borderRadius:5}}>
+          Login
+        </Button>
+        <Button 
+        size="large" 
+        variant="outlined" 
+        LinkComponent={Link} 
+        to="/register" 
+        sx={{borderRadius:5}} >
+          Register
+        </Button>
       </CardActions>
     </Card>
   );
--- src/pages/shared/LandingPage/LandingPage.jsx
+++ src/pages/shared/LandingPage/LandingPage.jsx
@@ -11,25 +11,33 @@ import FarmerBanner from "../../../assets/images/Banner-new.jpg";
 import Appbar from '../../../component/shared/Appbar';
 
 
+
 const landingpage = () => {
   
   return (
     
     <React.Fragment>
+    <Appbar/>
     <div className={noScrollbarsClassName}/>
       <div className={fullWidthClassName}/>
       <RemoveScrollBar/>
-    <Box style={{backgroundImage: FarmerBanner,}}>
-    <Grid>
-    <Box sx={{ display: 'flex' }}>
-      <Typography>
-
-      </Typography>
-        
-    </Box>
-    </Grid>
-    <Grid mt={20}>
-        <LandingPageCard/>
+    <Box sx={{
+      display: 'flex',
+      height:"100vh",
+      backgroundColor: 'primary.white',
+      backgroundImage: `url(${FarmerBanner})`,
+      backgroundRepeat  : 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'auto',
+      flexGrow: 1
+    }}>
+      <Grid container spacing={2}>
+        <Grid mt={25} xs={9} >
+          <LandingPageCard/>
+        </Grid>
+        <Grid mt={20} xs={3} >
+          
+        </Grid>
       </Grid>
     </Box>
     
