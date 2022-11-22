623d6ce552435fd71b3574c5a0e26efbe3898419
JohnArima <john.salangsang20@gmail.com>
2022-11-22 13:13:10 +0800

    new commit

--- src/pages/admin/DashboardPage/AdminDashboardPage.jsx
+++ src/pages/admin/DashboardPage/AdminDashboardPage.jsx
@@ -1,5 +1,7 @@
+import { Box, Container, Grid } from '@mui/material'
 import React from 'react'
 import AdminAppbar from '../../../component/admin/appbar/AdminAppbar'
+import DashboardCards from '../../../component/admin/Cards/Dashboard/DashboardCards'
 import AdminSidebar from '../../../component/admin/sidebar/AdminSidebar'
 import Appbar from '../../../component/shared/Appbar'
 
@@ -8,6 +10,11 @@ const AdminDashboardPage = () => {
     <>
       <Appbar/>
       <AdminSidebar/>
+      <Grid container mt={10}>
+        <Container>
+          <DashboardCards/>
+        </Container>
+      </Grid>
     </>
   )
 }
